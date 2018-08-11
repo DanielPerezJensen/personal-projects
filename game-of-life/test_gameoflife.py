e13178ff4c483ed17020c9ec5fd382445e440091
Daniel Perez Jensen <danielperezjensen@outlook.com>
2018-08-12 00:20:48 +0200

    added unittests for game of life and summarizer

--- game-of-life/test_gameoflife.py
+++ game-of-life/test_gameoflife.py
@@ -1,34 +1,59 @@
 import gameoflife as gol
+import unittest
 
-if __name__ == "__main__":
 
-    init_state1 = [[0, 0, 0],
-                   [0, 0, 0],
-                   [0, 0, 0]]
+class TestGameOfLife(unittest.TestCase):
+    def test_iteration(self):
+        # tests if iterations are correct
+        init_state1 = [[0, 0, 0],
+                       [0, 0, 0],
+                       [0, 0, 0]]
+        expected_next_state1 = [[0, 0, 0],
+                                [0, 0, 0],
+                                [0, 0, 0]]
+        actual_next_state1 = gol.iterate(init_state1)
 
-    expected_next_state1 = [[0, 0, 0],
-                            [0, 0, 0],
-                            [0, 0, 0]]
+        init_state2 = [[0, 0, 1],
+                       [0, 1, 1],
+                       [0, 0, 0]]
+        expected_next_state2 = [[0, 1, 1],
+                                [0, 1, 1],
+                                [0, 0, 0]]
+        actual_next_state2 = gol.iterate(init_state2)
 
-    actual_next_state1 = gol.iterate(init_state1)
+        self.assertEqual(expected_next_state1, actual_next_state1)
+        self.assertEqual(expected_next_state2, actual_next_state2)
 
-    if expected_next_state1 == actual_next_state1:
-        print('Passed 1')
-    else:
-        print('Failed 1')
-        print("Expected:\n {}".format(expected_next_state1))
-        print("Actual:\n {}".format(actual_next_state1))
+    def test_initialize_board(self):
+        board1 = gol.init(5, 5)
+        board2 = gol.init(4, 6)
+        board3 = gol.init(23, 1)
 
-    init_state2 = [[0, 0, 1],
-                   [0, 1, 1],
-                   [0, 0, 0]]
-    expected_next_state2 = [[0, 1, 1],
-                            [0, 1, 1],
-                            [0, 0, 0]]
-    actual_next_state2 = gol.iterate(init_state2)
-    if expected_next_state2 == actual_next_state2:
-        print('Passed 2')
-    else:
-        print('Failed 1')
-        print('Expected:\n'.format(expected_next_state2))
-        print('Actual:\n'.format(actual_next_state2))
+        self.assertEqual(len(board1), 5)
+        self.assertEqual(len(board1[0]), 5)
+
+        self.assertEqual(len(board2), 4)
+        self.assertEqual(len(board2[0]), 6)
+
+        self.assertEqual(len(board3), 23)
+        self.assertEqual(len(board3[0]), 1)
+
+    def test_loadboard(self):
+        expected_board_1 = [[0, 0, 0, 0, 0],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, 0, 0, 0]]
+
+        expected_board_2 = [[0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0],
+                            [0, 0, 1, 1, 1, 0],
+                            [0, 1, 1, 1, 0, 0],
+                            [0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0]]
+
+        actual_board_1 = gol.load_initial_state("beacon.txt")
+        actual_board_2 = gol.load_initial_state("toad.txt")
+
+        self.assertEqual(expected_board_1, actual_board_1)
+        self.assertEqual(expected_board_2, actual_board_2)
