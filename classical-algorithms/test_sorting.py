e6aaeecb30ac17e61f506eef29805b0f24cdace1
Daniel Perez Jensen <danielperezjensen@gmail.com>
2018-08-19 00:21:33 +0200

    fixed unittest

--- classical-algorithms/test_sorting.py
+++ classical-algorithms/test_sorting.py
@@ -2,17 +2,19 @@ import sorting as s
 import unittest
 import random
 
-unsorted_list = [random.randint(1, 20) for _ in range(10)]
-expected_sorted_list = sorted(unsorted_list)
-
 
 class TestSorters(unittest.TestCase):
+
     def test_bubblesort(self):
 
+        unsorted_list = [random.randint(1, 120) for _ in range(100)]
+        expected_sorted_list = sorted(unsorted_list)
         bubble_sorted_list = s.bubble_sort(unsorted_list)
         self.assertEqual(expected_sorted_list, bubble_sorted_list)
 
     def test_mergesort(self):
 
+        unsorted_list = [random.randint(1, 120) for _ in range(100)]
+        expected_sorted_list = sorted(unsorted_list)
         merge_sorted_list = s.merge_sort(unsorted_list)
         self.assertEqual(expected_sorted_list, merge_sorted_list)
