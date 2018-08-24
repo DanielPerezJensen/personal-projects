b4ad2c6b123d1496e14f90335fa08574f1cbb362
Daniel Perez Jensen <danielperezjensen@outlook.com>
2018-08-24 17:49:59 +0200

    converted to a class based system

--- gAImes/py-chess/pychess.py
+++ gAImes/py-chess/pychess.py
@@ -16,61 +16,59 @@ point_piece_dict = {
 }
 
 
-def init():
+class PyChess():
 
-    init_board = [[ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK],
-                  [PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN],
-                  [None, None, None, None, None, None, None, None],
-                  [None, None, None, None, None, None, None, None],
-                  [None, None, None, None, None, None, None, None],
-                  [None, None, None, None, None, None, None, None],
-                  [PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN],
-                  [ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK]]
+    def __init__(self):
 
-    init_colors = [["B", "B", "B", "B", "B", "B", "B", "B"],
-                   ["B", "B", "B", "B", "B", "B", "B", "B"],
-                   [None, None, None, None, None, None, None, None],
-                   [None, None, None, None, None, None, None, None],
-                   [None, None, None, None, None, None, None, None],
-                   [None, None, None, None, None, None, None, None],
-                   ["W", "W", "W", "W", "W", "W", "W", "W"],
-                   ["W", "W", "W", "W", "W", "W", "W", "W"]]
+        back_line = [ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK]
+        self.board = [back_line,
+                      [PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN],
+                      [None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None],
+                      [PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN],
+                      back_line]
 
-    return init_board, init_colors
+        self.colors = [["B", "B", "B", "B", "B", "B", "B", "B"],
+                       ["B", "B", "B", "B", "B", "B", "B", "B"],
+                       [None, None, None, None, None, None, None, None],
+                       [None, None, None, None, None, None, None, None],
+                       [None, None, None, None, None, None, None, None],
+                       [None, None, None, None, None, None, None, None],
+                       ["W", "W", "W", "W", "W", "W", "W", "W"],
+                       ["W", "W", "W", "W", "W", "W", "W", "W"]]
 
+        self.current_player = "W"
+        self.current_round = 1
 
-def calculate_board_value(board_state, board_colors, player="W"):
+    def calculate_board_value(self):
 
-    black_value = 0
-    white_value = 0
+        self.white_value = 0
+        self.black_value = 0
 
-    for i, row in enumerate(board_state):
-        for j, value in enumerate(row):
-            if value is not None:
-                if board_colors[i][j] == "W":
-                    white_value += value
-                elif board_colors[i][j] == "B":
-                    black_value += value
+        for i, row in enumerate(self.board):
+            for j, piece in enumerate(row):
+                if piece is not None:
+                    if self.colors[i][j] == "W":
+                        self.white_value += piece
+                    else:
+                        self.black_value += piece
 
-    if player == "W":
-        return white_value - black_value
-    elif player == "B":
-        return black_value - white_value
+    def render(self):
 
+        for row in self.board:
+            for i in range(len(row)):
+                print(point_piece_dict[row[i]], end=" ")
+            print()
 
-def render(board_state):
+    def switch(self):
 
-    for row in board_state:
-        for i in range(len(row)):
-            print(point_piece_dict[row[i]], end=" ")
-        print()
-
-
-def main():
-
-    initial_board_state, initial_board_colors = init()
-    render(initial_board_state)
+        if self.current_player == "W":
+            self.current_player = "B"
+        else:
+            self.current_player = "W"
 
 
-if __name__ == "__main__":
-    main()
+chess = PyChess()
+chess.render()
